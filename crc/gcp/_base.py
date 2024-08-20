f718bbd85f3fb1bd7b2ef33d4f3b96f3cc8f5607
Mohammad Lareb Zafar <zlareb1@gmail.com>
2024-08-20 18:56:54 +0530

    Update _base.py

--- crc/gcp/_base.py
+++ crc/gcp/_base.py
@@ -1,12 +1,12 @@
 # Copyright (c) Yugabyte, Inc.
 
-# A list of GCP regions
-GCP_REGION_LIST = [
-    "us-east1",
-    "asia-east1",
-    "asia-northeast1",
-    "asia-southeast1",
-    "europe-west1",
-    "us-central1",
-    "us-west1",
-]
+from google.cloud import compute_v1
+
+def get_gcp_regions(project_id):
+    client = compute_v1.RegionsClient()
+    project_id = project_id
+    regions = client.list(project=project_id)
+
+    # Extract region names
+    region_list = [region.name for region in regions]
+    return region_list
